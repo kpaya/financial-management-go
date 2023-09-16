6db2628f8b3b2d0cbf6c744bc1400cffee672ae5
Kpaya <matteus.santos03@gmail.com>
2023-09-16 14:53:18 -0300

    feat: add wallet domain

---
+++ src/domain/wallet.go
@@ -0,0 +1,25 @@
+package domain
+
+import "github.com/google/uuid"
+
+type Wallet struct {
+	WalletId     uuid.UUID
+	UserId       uuid.UUID
+	Transactions []Transaction
+	Active       bool
+}
+
+func NewWallet(userId uuid.UUID) *Wallet {
+	wallet := new(Wallet)
+	walletId, err := uuid.NewUUID()
+
+	if err != nil {
+		return wallet
+	}
+
+	wallet.WalletId = walletId
+	wallet.UserId = userId
+	wallet.Active = true
+
+	return wallet
+}
