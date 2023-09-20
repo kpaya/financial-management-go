078b330cda968b3aab18ef11dd06cb155addcbb2
Kpaya <matteus.santos03@gmail.com>
2023-09-19 22:42:28 -0300

    feat: add CreateNewUser usecase and DTO

---
+++ src/dto/user/dto.go
@@ -0,0 +1,12 @@
+package userDto
+
+import "github.com/google/uuid"
+
+type InputNewCreateUser struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type OutputNewCreateUser struct {
+	UserId uuid.UUID `json:"user_id"`
+}
---
+++ src/usecase/user/create.go
@@ -0,0 +1,35 @@
+package userUseCase
+
+import (
+	"github.com/kpaya/financial-management-go/src/domain"
+	userDto "github.com/kpaya/financial-management-go/src/dto/user"
+	repositoryInterface "github.com/kpaya/financial-management-go/src/repository"
+)
+
+type CreateNewUser struct {
+	Repository repositoryInterface.User
+}
+
+func NewCreateNewUser(repository repositoryInterface.User) (*CreateNewUser, error) {
+	return &CreateNewUser{
+		Repository: repository,
+	}, nil
+}
+
+func (u *CreateNewUser) Execute(input *userDto.InputNewCreateUser) (*userDto.OutputNewCreateUser, error) {
+	user, err := domain.NewUser(input.Email, input.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	err = u.Repository.InsertUser(user)
+	if err != nil {
+		return nil, err
+	}
+
+	output := &userDto.OutputNewCreateUser{
+		UserId: user.UserId,
+	}
+
+	return output, nil
+}
